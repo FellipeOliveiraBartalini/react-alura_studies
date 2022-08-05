9aa5fc9d14414555798b088b7a9a8970bb12c197
Fellipe de Oliveira <fellipeoliveiranobre@hotmail.com>
2022-08-05 13:00:34 -0300

    Controlando o form com o state

--- src/_components/form/index.tsx
+++ src/_components/form/index.tsx
@@ -3,24 +3,50 @@ import Button from '../button';
 import style from './form.module.scss';
 
 class Form extends React.Component {
-  render() {
-       return (
-           <form className={style.novaTarefa}>
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    };
+
+    adicionarTarefa(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        console.log('state > ', this.state);
+        
+    }
+
+    render() {
+        return (
+            <form className={style.novaTarefa} method="POST" onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
-                        <input type="text" name='tarefa' id='tarefa' placeholder='O que voçê quer estudar?' />
+                        <input
+                            type="text"
+                            name='tarefa'
+                            value={this.state.tarefa}
+                            onChange={event => {this.setState({ ...this.state, tarefa: event.target.value })}}
+                            id='tarefa'
+                            placeholder='O que voçê quer estudar?' />
                     </label>
                 </div>
 
                 <div className={style.inputContainer}>
                     <label htmlFor="tempo">
-                        <input type="time" step="1" name='tempo' id='tempo' min="00:00:00" max="01:30:00" required />
+                        <input
+                            type="time"
+                            step="1"
+                            name='tempo'
+                            value={this.state.tempo}
+                            onChange={event => {this.setState({ ...this.state, tempo: event.target.value })}}
+                            id='tempo'
+                            min="00:00:00"
+                            max="01:30:00"
+                            required />
                     </label>
                 </div>
                 <Button>Adicionar</Button>
-           </form>
-       )
+            </form>
+        )
     }
 }
 
