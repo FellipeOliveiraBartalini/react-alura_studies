89dcb37fe0f8474fc8c836140fe2e74b81ca7687
Fellipe de Oliveira <fellipeoliveiranobre@hotmail.com>
2022-08-05 09:26:03 -0300

    Add Sass

---
+++ alura-studies/src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from '../_components/form';
+import List from '../_components/list';
+import './style.scss';
+
+function App() {
+	return (
+		<div className="AppStyle">
+			<Form />
+			<List />
+		</div>
+	);
+}
+
+export default App;
