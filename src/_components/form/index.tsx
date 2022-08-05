afbabb57aefb8e60a7135f8a687d72c41179542a
Fellipe de Oliveira <fellipeoliveiranobre@hotmail.com>
2022-08-05 10:46:15 -0300

    Changed Folder

--- src/_components/form/index.tsx
+++ src/_components/form/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Button from '../button';
-import './style.scss';
+import style from './form.module.scss';
 
 class Form extends React.Component {
   render() {
        return (
-           <form className='novaTarefa'>
-                <div className="inputContainer">
+           <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor="tarefa">
                         Adicione um novo estudo
                         <input type="text" name='tarefa' id='tarefa' placeholder='O que voçê quer estudar?' />
                     </label>
                 </div>
 
-                <div className="inputContainer">
+                <div className={style.inputContainer}>
                     <label htmlFor="tempo">
                         <input type="time" step="1" name='tempo' id='tempo' min="00:00:00" max="01:30:00" required />
                     </label>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Form from '../_components/form';
 import List from '../_components/list';
-import './style.scss';
+import style from './app.module.scss';
 
 function App() {
 	return (
-		<div className="AppStyle">
+		<div className={style.AppStyle}>
 			<Form />
 			<List />
 		</div>
