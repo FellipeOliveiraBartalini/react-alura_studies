1e1fac61d4d4ee2f115c3b882ebd11f1e5d59dd6
Fellipe de Oliveira <fellipeoliveiranobre@hotmail.com>
2022-08-08 10:13:24 -0300

    --CleanCode-- Melhorado estrutura do css do item

--- src/_components/list/item/index.tsx
+++ src/_components/list/item/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ITarefa from '../../../types/tarefa.type';
-import style from '../list.module.scss';
+import style from './item.module.scss';
 
 interface Props extends ITarefa {
     selecionaTarefa: (tarefaSelecionada: ITarefa) => void
@@ -13,8 +13,6 @@ function Item({tarefa,
     id,
     selecionaTarefa
 }: Props) {
-    console.log('item atual > ', { tarefa, tempo, selecionado, completado, id });
-    
     return (
         <li className={`${style.item} ${selecionado ? style.itemSelecionado : null}`} onClick={() => selecionaTarefa(
             {
