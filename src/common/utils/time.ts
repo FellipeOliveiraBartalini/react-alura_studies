5545d829e6f357d490e48e91e5457a68b0f48082
Fellipe de Oliveira <fellipeoliveiranobre@hotmail.com>
2022-08-08 11:11:22 -0300

    --BugFix-- Não pegava minutos

--- src/common/utils/time.ts
+++ src/common/utils/time.ts
@@ -2,7 +2,7 @@ export function timeToSeconds(time: string) {
     const [hour = 0, minute = 0, seconds = 0] = time.split(':');
 
     const hours = Number(hour) * 3600;
-    const minutes = Number(hour) * 60;
+    const minutes = Number(minute) * 60;
 
     return hours + minutes + Number(seconds);
 }
